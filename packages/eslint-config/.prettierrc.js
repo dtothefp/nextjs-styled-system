d3a63a54e8cdba213a01d094f3e4c11783030a06
dtothefp <dtothefp@gmail.com>
2021-04-03 08:09:54 -0700

    fix issue for isolated modules in jest

---
+++ packages/eslint-config/.prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  printWidth: 120,
+  semi: true,
+  useTabs: false,
+  singleQuote: true,
+  trailingComma: 'es5',
+  bracketSpacing: true,
+  jsxBracketSameLine: true,
+  arrowParens: 'always',
+};
