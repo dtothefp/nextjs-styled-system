2dfa5122568adadd80566b31fd19a98b4ae3f08a
dtothefp <dtothefp@gmail.com>
2021-04-11 21:18:57 -0700

    Update 2021-04-11 21:18:50

---
+++ _next/static/5iW-8yWvmudTwW91ZYzwc/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-49e53ca0062ceaa23f75.js"],"/_error":["static/chunks/pages/_error-501820c9abdeb8afa44d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
